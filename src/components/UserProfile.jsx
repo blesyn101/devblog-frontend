d21c75094ad4d46b9bf44cb3485880f18f03774e
Agharese Blessing <blessing4coding@gmail.com>
2025-11-17 11:29:19 +0100

    add user-profile

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserProfile() {
+  return (
+    <div>
+        <img src="https://via.placeholder.com/150" alt="User Avatar" />
+        <h2>John Doe</h2>
+        <p>bio</p>
+    </div>
+  )
+}
+
+export default UserProfile
